20614bbecf93258fbd81b1273ddb37ace87f7c92
Miguel C <webdazer0@gmail.com>
2021-05-01 12:56:46 +0200

    added backgound in HomePage

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -4,8 +4,33 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
       
+      body: Stack(
+
+        fit: StackFit.expand,
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              'assets/welcome.jpg',
+              fit: BoxFit.cover,
+            ),
+          ),
+
+          SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Image.asset(
+                  'assets/logo.png', 
+                  height: 50,
+                ),
+              ],
+            ),
+          )
+
+        ],
+      ),
     );
   }
 }
