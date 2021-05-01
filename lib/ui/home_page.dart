30dab8f0ba374d5f635b0aef838855826945165d
Miguel C <webdazer0@gmail.com>
2021-05-01 17:26:53 +0200

    added raisedButton & ElevatedButton

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -3,7 +3,20 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    // Per visualizza l'alto e l'altezza del dispositivo
     print(MediaQuery.of(context).size);
+
+    //Style custom per far assomigliare un ElevatedButton a quello deprecato RaisedButton
+    final ButtonStyle raisedButtonStyle = ElevatedButton.styleFrom(
+      onPrimary: Colors.black,
+      primary: Colors.grey[300],
+      minimumSize: Size(88, 36),
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(15)),
+        ),
+    );
 
     return Scaffold(
       body: Stack(
@@ -18,7 +31,7 @@ class HomePage extends StatelessWidget {
           SafeArea(
             child: Center(
               child: Container(
-                color: Colors.red,
+                // color: Colors.red,
                 constraints: BoxConstraints(maxWidth: 250.0),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -30,18 +43,50 @@ class HomePage extends StatelessWidget {
                       'assets/logo.png',
                       height: 50,
                     ),
+                    const SizedBox(
+                      height: 80,
+                    ),
                     Text(
                       'Hola, \nBienvenido',
                       textAlign: TextAlign.start,
                       style: TextStyle(
                           fontSize: 30,
                           fontWeight: FontWeight.bold,
-                          backgroundColor: Colors.red[200]),
+                      ),
                     ),
-                    Text(
-                      'Que te parece si agregamos \nuna nueva ciudad?',
-                      textAlign: TextAlign.start,
+                    const SizedBox(
+                      height: 50,
                     ),
+                    GestureDetector(
+                      onTap: () => print('Ciaooooo, hai premuto su un Gesture Detector'),
+                      child: Text(
+                        'Que te parece si agregamos \nuna nueva ciudad?',
+                        textAlign: TextAlign.start,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    RaisedButton(
+                      onPressed: () => print('Ciao'),
+                      elevation: 0,
+                      splashColor: Color(0xFF346FFF),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(
+                          15.0,
+                        ),
+                      ),
+                      child: Text(
+                        'Aggiungi Città'
+                      )
+                    ),
+                    ElevatedButton(
+                      onPressed: () => print('Ciaoxxxxxx'),
+                      style: raisedButtonStyle,
+                      child: Text(
+                        'Aggiungi Città 2'
+                      )
+                    )
                   ],
                 ),
               ),
