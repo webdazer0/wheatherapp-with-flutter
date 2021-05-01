834cddf5640f9cdcf20fa0ef2cef53a21c31db44
Miguel C <webdazer0@gmail.com>
2021-05-01 16:32:31 +0200

    added Widget Text & Logo

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      
-      body: Stack(
+    print(MediaQuery.of(context).size);
 
+    return Scaffold(
+      body: Stack(
         fit: StackFit.expand,
         children: [
           Positioned.fill(
@@ -16,19 +15,38 @@ class HomePage extends StatelessWidget {
               fit: BoxFit.cover,
             ),
           ),
-
           SafeArea(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Image.asset(
-                  'assets/logo.png', 
-                  height: 50,
+            child: Center(
+              child: Container(
+                color: Colors.red,
+                constraints: BoxConstraints(maxWidth: 250.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    Image.asset(
+                      'assets/logo.png',
+                      height: 50,
+                    ),
+                    Text(
+                      'Hola, \nBienvenido',
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          backgroundColor: Colors.red[200]),
+                    ),
+                    Text(
+                      'Que te parece si agregamos \nuna nueva ciudad?',
+                      textAlign: TextAlign.start,
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
           )
-
         ],
       ),
     );
