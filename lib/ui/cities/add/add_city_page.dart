e3eeb7d4a6c010889e5be62ae40be9bce6673cd4
Miguel C <webdazer0@gmail.com>
2021-05-02 02:29:34 +0200

    added ListView for cities & model City

--- lib/ui/cities/add/add_city_page.dart
+++ lib/ui/cities/add/add_city_page.dart
@@ -1,6 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:wheatherapp/model/City.dart';
 import 'package:wheatherapp/ui/common/header_widget.dart';
 import 'package:wheatherapp/ui/common/debouncer.dart';
+import 'package:http/http.dart' as http;
+import 'package:wheatherapp/ui/ui_constants.dart';
 
 class AddCityPage extends StatefulWidget {
   @override
@@ -10,10 +15,39 @@ class AddCityPage extends StatefulWidget {
 class _AddCityPageState extends State<AddCityPage> {
   final debouncer = Debouncer();
 
+  List<City> cities = [];
+  bool loading = false;
+
+  final String uri = 'https://api.openweathermap.org/data/2.5/weather/';
+  final String apiKey = '737a55574b6c9d2e464d6307f8469006';
+  final String celsius = 'metric';
+
   void onChangedText(String text) {
     debouncer.run(() {
-      if(text.length > 3) print('text:  $text');
+      if (text.isNotEmpty) {
+        requestSearch(text);
+        print('text:  $text');
+      }
     });
+  }
+
+  void requestSearch(String city) async {
+    setState(() {
+      loading = true;
+    });
+
+    final url = '$uri?appid=$apiKey&units=$celsius&q=$city&lang=it';
+    final url2 = 'https://www.metaweather.com/api/location/search/?query=$city';
+
+    final res = await http.get(Uri.parse(url2));
+    final data = jsonDecode(res.body) as List;
+
+    setState(() {
+      cities = data.map((e) => City.fromJson(e)).toList();
+      loading = false;
+    });
+    print(res.body);
+    print(cities);
   }
 
   @override
@@ -53,6 +87,37 @@ class _AddCityPageState extends State<AddCityPage> {
                   ),
                 ),
               ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: cities.length,
+                itemBuilder: (context, index) {
+                  print(cities.length);
+                  final city = cities[index];
+                  return ListTile(
+                    title: Text(
+                      city.title,
+                      style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    trailing: IconButton(
+                      icon: Icon(
+                        Icons.add,
+                        color: primaryColor,
+                      ),
+                      onPressed: () => print("HEEY"),
+                    ),
+                  );
+                },
+              ),
+            ),
+            if(loading)
+            Center(
+              child: CircularProgressIndicator(),
             ),
           ],
         ),
